6f51067e7a3aa07b29e4f6bcc16bd75bc13e21da
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-22 01:02:03 +0600

    full join implemented

--- join.sql
+++ join.sql
@@ -4,5 +4,19 @@
 SELECT * FROM "user";
 SELECT * FROM post
 
-SELECT * from post as p
-JOIN "user" as u on p.user_id = u.id
+SELECT * from post
+JOIN "user" on "user".id = post.id
+
+INSERT INTO post(id, title, user_id)
+VALUES
+(5, 'this is a test post title', NULL)
+
+SELECT * from post
+LEFT OUTER JOIN "user" on post.user_id = "user".id;
+
+SELECT * from post
+RIGHT OUTER JOIN "user" on post.user_id = "user".id;
+
+SELECT * from post
+FULL OUTER JOIN "user" on post.user_id = "user".id;
+
